5d23626f42ccf0a79133ca857f95ffa54f5e47c7
Matteo Menapace <iosonomatteo@gmail.com>
2012-12-16 21:19:09 +0000

    Added timer and switch mission logic

--- js/game.js
+++ js/game.js
@@ -1,10 +1,9 @@
-(function() {
-    //
-    // Components
-    //
+(function() 
+{
 
     // a renderable entity
-    Crafty.c('Renderable', {
+    Crafty.c('Renderable', 
+    {
         init: function() {
             // we're using DOM Spirtes
             this.requires('2D, DOM');
@@ -14,10 +13,11 @@
             this.requires(name);
             return this; // so we can chain calls to setup functions
         } 
-    });
+    })
 
     // a component to fade out an entity over time
-    Crafty.c('FadeOut', {
+    Crafty.c('FadeOut', 
+    {
         init: function() {
             this.requires('2D');
 
@@ -37,10 +37,11 @@
             this._fadeSpeed = speed;
             return this; // so we can chain calls to setup functions
         }
-    });
+    })
 
     // rotate an entity continually
-    Crafty.c('Rotate', {
+    Crafty.c('Rotate', 
+    {
         init: function() {
             this.requires('2D');
 
@@ -56,20 +57,22 @@
             this._rotationSpeed = speed;
             return this; // so we can chain calls to setup functions
         },
-    });
+    })
 
     // an exciting explosion!
-    Crafty.c('Explosion', {
+    Crafty.c('Explosion', 
+    {
         init: function() {
             // reuse some helpful components
             this.requires('Renderable, FadeOut')
                 .spriteName('explosion' + Crafty.math.randomInt(1,2))
                 .fadeOut(0.1);
         }
-    });
+    })
 
     // a bullet, it shoots things
-    Crafty.c('Bullet', {
+    Crafty.c('Bullet', 
+    {
         init: function() {
             this.requires('Renderable, Collision, Delay, SpriteAnimation')
                 .spriteName('bullet')
@@ -86,11 +89,11 @@
                     }
                 })
         }
-    });
-
-    
+    })
+   
     // targets to shoot at
-    Crafty.c('Target', {
+    Crafty.c('Target', 
+    {
         init: function() {
             this.requires('Renderable, Collision, Delay')
                 // choose a random enemy sprite to use
@@ -122,10 +125,11 @@
             // reappear after a second in a new position
             this.delay(this._randomlyPosition, 1000);
         },
-    });
+    })
 
     // Limit movement to within the viewport
-    Crafty.c('ViewportBounded', {
+    Crafty.c('ViewportBounded', 
+    {
         init: function() {
             this.requires('2D');
         },
@@ -135,35 +139,7 @@
                 this.attr({x: oldPosition.x, y: oldPosition.y});
             }
         }
-    });
-
-    // Player component    
-    /*Crafty.c('Player', {        
-        init: function() {           
-            this.requires('Renderable, Fourway, Collision, ViewportBounded, SpriteAnimation')
-                .spriteName('player')
-                .collision()
-                .attr({x: 64, y: 64})
-                // animate the ship - set up animation, then trigger it
-                .animate('fly', 0, 0, 1)
-                .animate('fly', 5, -1)
-                // set up fourway controller
-                .fourway(5)
-                // also react to the SPACE key being pressed
-                .requires('Keyboard')
-                .bind('KeyDown', function(e) {
-                    if (e.key === Crafty.keys.SPACE) {
-                        // fire bullet
-                        Crafty.e("Bullet").attr({x: this.x + 5, y: this.y});
-                    }
-                });
-
-            // bind our movement handler to keep us within the Viewport
-            this.bind('Moved', function(oldPosition) {
-                this.checkOutOfBounds(oldPosition);
-            });
-        },
-    });*/
+    })
 
     // Player component    
     Crafty.c('Player', 
@@ -233,11 +209,117 @@
 
             return this
         }
+    })
 
+    Crafty.c('GameLogic', 
+    {
+        timer: 0,
+
+        switchTypes:
+        [
+            'mission',
+            // 'roles'
+        ],
+
+        init: function() 
+        {
+            this.requires('Delay')
+            this.setMission()
+            this.reset()
+            // this.tick()
+        },
+        
+        tick: function() 
+        {
+            // TODO check for game over
+
+            if (this.timer <= 0) 
+            {
+                this.reset()
+                this.switch()
+            }
+
+            this.timer = this.timer - 1
+
+            $('#timer').html(this.timer)
+
+            this.delay(this.tick, 1000)
+        },
+
+        reset: function()
+        {
+            this.timer = Crafty.math.randomInt(2,3)
+        },
+
+        switch: function()
+        {
+            // roles or mission?
+            var randomType = this.switchTypes[ Math.round(Math.random() * (this.switchTypes.length - 1)) ]
+
+            
+            // var number = Math.round(Math.random())
+            // var type = (number == 1) ? 'roles' : 'mission'
+
+            // console.log('switch ' + randomType)
+
+            switch (randomType)
+            {
+                case 'mission':
+                    this.switchMission()
+                    this.setMission()
+                    break
+
+                case 'roles':
+                    this.setRoles()
+                    break   
+            }
+        },
+
+        setMission: function()
+        {
+            _.each(battle.players, function(player, index)
+            {
+                // console.log(player.name + '[' + index + '] becomes ' + battle.mission[index])
+                player.status = battle.mission[index]
+
+                var span = $('#gameScreen h1 span.' + player.status)
+                span.html(player.name)
+            })
+
+            gameMission.effect('highlight', {}, 1000) 
+        },
+
+        switchMission: function()
+        {
+            battle.mission.reverse()
+        },
+
+        setRoles: function()
+        {
+            /*Crafty("Player").each(function () {
+                    //console.log(this); 
+                    // this.switchRole();
+                })*/
+        },
+
+        shuffle: function(array)
+        {
+            // console.log('shuffle')
+            // console.log(array)
+            for (var j, x, i = array.length; i; j = parseInt(Math.random() * i), x = array[--i], array[i] = array[j], array[j] = x)
+            // console.log(array)
+            return array
+        }
+
+        /*hide: function()
+        {
+            this.text = ''
+        }*/
     })
 
     // A component to display the player's score
-    Crafty.c('Score', {
+    Crafty.c('Score', 
+    {
         init: function() {
             this.score = 0;
             this.requires('2D, DOM, Text');
@@ -254,18 +336,16 @@
         }
     })
 
-
-    //
-    // Game loading and initialisation
-    //    
-    var Game = function() {
-        Crafty.scene('loading', this.loadingScene);
+    // Game loading and initialisation    
+    var Game = function() 
+    {
+        // Crafty.scene('loading', this.loadingScene);
         Crafty.scene('main', this.mainScene);
     };
     
     Game.prototype.initCrafty = function() 
     {
-        console.log("page ready, starting CraftyJS")
+        console.log('Game.prototype.initCrafty')
         Crafty.init(config.stage.width, config.stage.height)
         Crafty.canvas.init()
         
@@ -274,65 +354,29 @@
                Crafty.debugBar.show();
             }
         });*/
-    };
-    
-    // A loading scene -- pull in all the slow things here and create sprites
-    Game.prototype.loadingScene = function() {
-        var loading = Crafty.e('2D, Canvas, Text, Delay');
-        loading.attr({x: 512, y: 200, w: 100, h: 20});
-        loading.text('loading...');
-        
-        function onLoaded() {
-            // set up sprites
-            Crafty.sprite(64, 'img/shooter-sprites.png', {
-                player: [0, 0],
-                bullet: [0, 1],
-                enemy1: [0, 2],
-                enemy2: [1, 2],
-                explosion1: [0, 3],
-                explosion2: [1, 3]
-                });
-            
-            // jump to the main scene in half a second
-            loading.delay(function() {
-                Crafty.scene('main');
-            }, 500);
-        }
-        
-        function onProgress(progress) {
-            loading.text('loading... ' + progress.percent + '% complete');
-        }
-        
-        function onError() {
-            loading.text('could not load assets');
-        }
-        
-        Crafty.load([
-            // list of images to load
-            'img/shooter-sprites.png'
-        ], 
-        onLoaded, onProgress, onError);
-        
-    };
+    }
     
     Game.prototype.mainScene = function() 
     {
         // create a scoreboard
         // Crafty.e('Score')
 
+        Crafty.e('GameLogic')
+        // Crafty.e('GameOver')
+
         //create players...
 
         // left
         var playerLeft = battle.players[0]
         Crafty.e('Player')
-            .initControls({speed:5, controls:{W: -90, S: 90, D: 0, A: 180}})
+            .initControls({speed:config.player.speed, controls:{W: -90, S: 90, D: 0, A: 180}})
             .setCharacter(playerLeft.src, {w:config.player.width, h:config.player.height})
             .setPosition({x:0, y:0})
 
         // right
         var playerRight = battle.players[1]
         Crafty.e('Player')
-            .initControls({speed:5, controls:{UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180}})
+            .initControls({speed:config.player.speed, controls:{UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180}})
             .setCharacter(playerRight.src, {w:config.player.width, h:config.player.height})
             .setPosition({x:Crafty.viewport.width - config.player.width, y:Crafty.viewport.height - config.player.height}) 
         
@@ -361,13 +405,17 @@
         imageBtn, 
         rawImage,
         previewImage,
-        gameScreen
+        gameScreen,
+        gameMission
+        // chaserSpan,
+        // chasedSpan
 
     // the game data object
     var battle = 
     {
         title: '', 
-        players: [] 
+        players: [], // name, url, status (chased | chaser), control (left | right)
+        mission: ['chaser', 'chased'] 
     }
 
     var config =
@@ -375,11 +423,12 @@
         stage:
         {
             width: 940,
-            height: 445
+            height: 448
         },
 
         player:
         {
+            speed: 4,
             width: 64,
             height: 64
         }
@@ -434,7 +483,7 @@
            
        var players = battle.title.split(' vs ')
        battle.players = []
-        _.each(players, function(player)
+        _.each(players, function(player, index)
         {
             var name = player
             var src = 'img/' + player.toLowerCase() + '.png'
@@ -474,6 +523,9 @@
     function step3()
     {
         gameScreen = $('#gameScreen')
+        gameMission = $('#gameScreen h1')
+        // chaserSpan = $('#gameScreen h1 span.chaser')
+        // chasedSpan = $('#gameScreen h1 span.chased')
 
         optionsScreen.hide()
         gameScreen.show()
@@ -482,7 +534,84 @@
         game.initCrafty({w:config.stage.width, h:config.stage.height})
 
         // start loading things
-        Crafty.scene('loading')
+        // Crafty.scene('loading')
+
+        Crafty.scene('main')
     }
+
+
+
+
+    
+    // A loading scene -- pull in all the slow things here and create sprites
+    /*Game.prototype.loadingScene = function() 
+    {
+        var loading = Crafty.e('2D, Canvas, Text, Delay');
+        loading.attr({x: 512, y: 200, w: 100, h: 20});
+        loading.text('loading...');
+        
+        function onLoaded() {
+            // set up sprites
+            Crafty.sprite(64, 'img/shooter-sprites.png', {
+                player: [0, 0],
+                bullet: [0, 1],
+                enemy1: [0, 2],
+                enemy2: [1, 2],
+                explosion1: [0, 3],
+                explosion2: [1, 3]
+                });
+            
+            // jump to the main scene in half a second
+            loading.delay(function() {
+                Crafty.scene('main');
+            }, 500);
+        }
+        
+        function onProgress(progress) {
+            loading.text('loading... ' + progress.percent + '% complete');
+        }
+        
+        function onError() {
+            loading.text('could not load assets');
+        }
+        
+        Crafty.load([
+            // list of images to load
+            'img/shooter-sprites.png'
+        ], 
+        onLoaded, onProgress, onError);
+        
+    };*/
+
+    // Player component    
+    /*Crafty.c('Player', {        
+        init: function() {           
+            this.requires('Renderable, Fourway, Collision, ViewportBounded, SpriteAnimation')
+                .spriteName('player')
+                .collision()
+                .attr({x: 64, y: 64})
+                // animate the ship - set up animation, then trigger it
+                .animate('fly', 0, 0, 1)
+                .animate('fly', 5, -1)
+                // set up fourway controller
+                .fourway(5)
+                // also react to the SPACE key being pressed
+                .requires('Keyboard')
+                .bind('KeyDown', function(e) {
+                    if (e.key === Crafty.keys.SPACE) {
+                        // fire bullet
+                        Crafty.e("Bullet").attr({x: this.x + 5, y: this.y});
+                    }
+                });
+
+            // bind our movement handler to keep us within the Viewport
+            this.bind('Moved', function(oldPosition) {
+                this.checkOutOfBounds(oldPosition);
+            });
+        },
+    });*/
+
+
+
     
 })()

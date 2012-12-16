95642d82f110f3bf5b729fbbc273eeea597d16e5
Matteo Menapace <iosonomatteo@gmail.com>
2012-12-16 16:59:05 +0000

    Game intro and pairs selection flow

--- js/game.js
+++ js/game.js
@@ -138,7 +138,7 @@
     });
 
     // Player component    
-    Crafty.c('Player', {        
+    /*Crafty.c('Player', {        
         init: function() {           
             this.requires('Renderable, Fourway, Collision, ViewportBounded, SpriteAnimation')
                 .spriteName('player')
@@ -163,7 +163,78 @@
                 this.checkOutOfBounds(oldPosition);
             });
         },
-    });
+    });*/
+
+    // Player component    
+    Crafty.c('Player', 
+    {        
+        init: function() 
+        {  
+            // this.requires('Renderable, Fourway, Collision, ViewportBounded, SpriteAnimation, Directions')
+            this.requires('Collision, ViewportBounded, Multiway')
+                .collision()
+                // .attr({x: 64, y: 64})
+                // animate the ship - set up animation, then trigger it
+                // .animate('fly', 0, 0, 1)
+                // .animate('fly', 5, -1)
+
+            // bind our movement handler to keep us within the Viewport
+            this.bind('Moved', function(oldPosition) 
+            {
+                this.checkOutOfBounds(oldPosition)
+            })    
+        },
+
+        initControls: function(config)
+        {
+            // console.log('Player initControls')
+            // console.log(config) 
+
+            // set up multiway controller
+            this.multiway(config.speed, config.controls)
+
+            // also react to the SPACE key being pressed
+            /*.requires('Keyboard')
+            .bind('KeyDown', function(e) 
+            {
+                if (e.key === Crafty.keys.SPACE) 
+                {
+                    // fire bullet
+                    Crafty.e("Bullet").attr({x: this.x + 5, y: this.y});
+                }
+            })*/
+
+            /*this.bind('NewDirection', function(direction) 
+            {
+                this.checkDirection(direction)
+            })*/
+
+            return this
+        },
+
+        setCharacter: function(image, size) // url or base64 string
+        {
+
+            // what if we have a character already attached?
+
+            this.character = Crafty.e('2D, DOM, Image')
+                .image(image)
+                .attr({w: size.w, h: size.h})
+
+            this.attach(this.character)
+            this.attr({w: size.w, h: size.h})
+
+            return this
+        },
+
+        setPosition: function(position)
+        {
+            this.attr({x: position.x, y: position.y})
+
+            return this
+        }
+
+    })
 
     // A component to display the player's score
     Crafty.c('Score', {
@@ -192,16 +263,17 @@
         Crafty.scene('main', this.mainScene);
     };
     
-    Game.prototype.initCrafty = function() {
-        console.log("page ready, starting CraftyJS");
-        Crafty.init(1000, 600);
-        Crafty.canvas.init();
+    Game.prototype.initCrafty = function() 
+    {
+        console.log("page ready, starting CraftyJS")
+        Crafty.init(config.stage.width, config.stage.height)
+        Crafty.canvas.init()
         
-        Crafty.modules({ 'crafty-debug-bar': 'release' }, function () {
+        /*Crafty.modules({ 'crafty-debug-bar': 'release' }, function () {
             if (Crafty.debugBar) {
                Crafty.debugBar.show();
             }
-        });
+        });*/
     };
     
     // A loading scene -- pull in all the slow things here and create sprites
@@ -243,29 +315,174 @@
         
     };
     
-    //
-    // The main game scene
-    //
-    Game.prototype.mainScene = function() {
+    Game.prototype.mainScene = function() 
+    {
         // create a scoreboard
-        Crafty.e('Score');
+        // Crafty.e('Score')
 
-        //create a player...
-        Crafty.e('Player');
+        //create players...
+
+        // left
+        var playerLeft = battle.players[0]
+        Crafty.e('Player')
+            .initControls({speed:5, controls:{W: -90, S: 90, D: 0, A: 180}})
+            .setCharacter(playerLeft.src, {w:config.player.width, h:config.player.height})
+            .setPosition({x:0, y:0})
+
+        // right
+        var playerRight = battle.players[1]
+        Crafty.e('Player')
+            .initControls({speed:5, controls:{UP_ARROW: -90, DOWN_ARROW: 90, RIGHT_ARROW: 0, LEFT_ARROW: 180}})
+            .setCharacter(playerRight.src, {w:config.player.width, h:config.player.height})
+            .setPosition({x:Crafty.viewport.width - config.player.width, y:Crafty.viewport.height - config.player.height}) 
         
         // create some junk to avoid
-        for (i = 0; i < 5; i++) {
-            Crafty.e('Target');
+        /*for (i = 0; i < 5; i++) 
+        {
+            Crafty.e('Target')
+        }*/
+    }
+
+
+
+
+
+
+
+
+    // DOM elements references
+    var step2Btn, 
+        introScreen,
+        step3Btn,
+        optionsScreen,
+        pairsGroup,
+        playersAvatars,
+        modalPicture,
+        imageBtn, 
+        rawImage,
+        previewImage,
+        gameScreen
+
+    // the game data object
+    var battle = 
+    {
+        title: '', 
+        players: [] 
+    }
+
+    var config =
+    {
+        stage:
+        {
+            width: 940,
+            height: 445
+        },
+
+        player:
+        {
+            width: 64,
+            height: 64
         }
-    };
+    }
+
+    // kick off when the web page is ready
+    $(document).ready(function() 
+    {
+        step1()    
+    })
+
+    function step1() // intro
+    {
+        console.log('step1')
+
+        introScreen = $('#introScreen')
+        step2Btn = $('#step2Btn')
+
+        step2Btn.on('click', step2)
+    }
     
-    // kick off the game when the web page is ready
-    $(document).ready(function() {
-        var game = new Game();
-        game.initCrafty();
+    function step2() // game options    
+    {
+        console.log('step2')
+
+        optionsScreen = $('#optionsScreen')
+        pairsGroup = $('#pairsGroup')
+        playersAvatars = $('#playersAvatars')
+        step3Btn = $('#step3Btn')
+
+        introScreen.hide()
+        optionsScreen.show()
+
+        $('a.pairs-btn').on('click', pairChosen)
+
+
+        // modalPicture = $('#pictureModal')
+        // imageBtn = $('#imageBtn')
+        // rawImage = $('img.raw')
+        // previewImage = $('img.preview')
+        // imageUrl = $('#imageUrl')
         
+    }
+
+    function pairChosen(event)
+    {
+       var a = $(this)
+
+       battle.title = a.html()
+
+       // console.log('pairChosen ' + battle.title)
+           
+       var players = battle.title.split(' vs ')
+       battle.players = []
+        _.each(players, function(player)
+        {
+            var name = player
+            var src = 'img/' + player.toLowerCase() + '.png'
+            battle.players.push({name: name, src:src})
+        })
+
+        showTitle()
+        showAvatars()
+        enableStep3Button()
+    }
+
+    function showTitle()
+    {
+        $('#pairsGroup button .cta').html(battle.title)
+    }
+
+    function showAvatars()
+    {
+        playersAvatars.show()
+
+        showAvatar('#playersAvatars .pull-left', battle.players[0]) // left    
+        showAvatar('#playersAvatars .pull-right', battle.players[1]) // right
+    }
+
+    function showAvatar(selector, player)
+    {
+        $(selector).find('img').attr('src', player.src)
+        $(selector).find('h3').html(player.name)
+    }
+
+    function enableStep3Button()
+    {
+        step3Btn.removeClass('disabled')
+        step3Btn.on('click', step3)
+    }
+
+    function step3()
+    {
+        gameScreen = $('#gameScreen')
+
+        optionsScreen.hide()
+        gameScreen.show()
+
+        var game = new Game();
+        game.initCrafty({w:config.stage.width, h:config.stage.height})
+
         // start loading things
-        Crafty.scene('loading');
-    });
+        Crafty.scene('loading')
+    }
     
-})();
+})()
